531239c43efc7c661034c2c720dc22618f4dae87
Ragab Hassan <r.elkattawy@gmail.com>
2025-05-01 18:43:10 +0300

    Add license and License information
    Signed-off-by: Ragab Hassan <r.elkattawy@gmail.com>

--- HelloWorld/src/HelloWorldClient.cpp
+++ HelloWorld/src/HelloWorldClient.cpp
@@ -1,3 +1,14 @@
+/**
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (C) 2025 Ragab R. Hassan (r.elkattawy@gmail.com)
+ * The source of this file can be found here:
+ * https://github.com/COVESA/capicxx-core-tools/wiki/Loading-Bindings-And-Libraries  
+ * 
+ */
 #include <iostream>
 #include <string>
 #include <thread>
