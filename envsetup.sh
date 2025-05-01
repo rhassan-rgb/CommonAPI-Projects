234adeae4e410eeff30fccbefeb506c36a964744
Ragab Hassan <r.elkattawy@gmail.com>
2025-05-01 20:31:52 +0300

    Add license info to envsetup
    Signed-off-by: Ragab Hassan <r.elkattawy@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -1,3 +1,13 @@
+# #############################################################################
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# 
+# Copyright (C) 2025 Ragab R. Hassan (r.elkattawy@gmail.com)
+#
+# #############################################################################
+
 # Directories
 ABS_ROOT_WD=$(realpath .)
 ROOT_WD=$(pwd)
